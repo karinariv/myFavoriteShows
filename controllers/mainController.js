d78f641436575fa1790b8cc05e27f9f2fe0f19fc
karinariv <karinag_riverag@hotmail.com>
2021-10-03 10:42:57 -0500

    Project almost completed

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,11 +1,38 @@
 const { response } = require("express");
+let movies = require('../data/dbMovies.json');
+let series = require('../data/dbSeries.json');
+let documentaries = require('../data/dbDocumentaries.json');
 
 const controller = {
     home: (req, res) => {
-        res.render('index');
+        res.render('index', {movies: movies, series: series, documentaries: documentaries});
     },
-    detail: (req, res) => {
-        res.render('detail');
+    detailMovie: (req, res) => {
+        let s = '';
+        let d = '';
+        for (let movie of movies) {
+            if (movie.id == req.params.id){
+                return res.render('detail', {movie: movie, series: s, documentary: d});
+            }
+        }   
+    },
+    detailSeries: (req, res) => {
+        let m = '';
+        let d = '';
+        for (let serie of series) {
+            if (serie.id == req.params.id){
+                return res.render('detail', {series: serie, movie: m, documentary: d});
+            }
+        }   
+    },
+    detailDocumentary: (req, res) => {
+        m = '';
+        s = '';
+        for (let documentary of documentaries) {
+            if (documentary.id == req.params.id){
+                return res.render('detail', {documentary: documentary, series: s, movie: m});
+            }
+        }   
     },
     about: (req, res) => {
         res.render('about');
--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const mainController = require('../controllers/mainController');
 
 router.get('/', mainController.home);
-router.get('/movieDetail', mainController.detail);
+router.get('/movieDetail/:id', mainController.detailMovie);
+router.get('/seriesDetail/:id', mainController.detailSeries);
+router.get('/documentaryDetail/:id', mainController.detailDocumentary);
 router.get('/about', mainController.about);
 
 module.exports = router;
